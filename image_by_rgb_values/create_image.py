4cb25a32b1f9c079726e064736a29a4f8948f8aa
buldozzzer <48224149+buldozzzer@users.noreply.github.com>
2024-03-26 10:44:28 +0300

    Update create_image.py

--- image_by_rgb_values/create_image.py
+++ image_by_rgb_values/create_image.py
@@ -14,7 +14,7 @@ height = 853
 
 img  = Image.new( mode = "RGB", size = (width, height))
 
-# Название файла со значениями пикселей можно изменить
+# Название файла со значениями пикселей, можно изменить
 with open("1280-853.txt", "r") as f:
     for line in f:
         values = re.findall(r'\d+', line)
